13037bbf594a60e487b246342a9e044bec8b0603
KevinManimau <kevin03manimau@gmail.com>
2020-02-07 14:48:36 +0800

    menambahkan api dan component

---
+++ src/components/Table.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export default class Table extends React.Component{
+    
+    constructor(){
+        super();
+        this.state=({
+            db:[]
+        });
+    }
+
+    getAllUser(){
+        fetch("")
+    }
+    
+    render(){
+        return(
+            <div>
+
+            </div>
+        );
+    }
+}
